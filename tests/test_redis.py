21ef97c8fb32c5fb140eb70981b848a07cf81a72
senkovskiy <senkovskiy@gmail.com>
2025-03-30 21:09:25 +0200

    change redis test

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -1,5 +1,13 @@
 import pytest
 from httpx import AsyncClient
+from fastapi import FastAPI
+from api.app import get_application
+from asgi_lifespan import LifespanManager
+
+
+import pytest
+from httpx import AsyncClient
+from fastapi.testclient import TestClient
 from asgi_lifespan import LifespanManager
 from api.app import get_application
 
@@ -9,9 +17,10 @@ async def test_redis_ping():
     app = get_application()
 
     async with LifespanManager(app):
-        async with AsyncClient(app=app, base_url="http://test") as client:
-            res = await client.get("/api/v1/redis/ping")
-            assert res.status_code == 200
-            data = res.json()
-            assert data["status"] == "OK"
-            assert data["ping"] == "pong"
+        with TestClient(app) as sync_client:
+            async with AsyncClient(base_url="http://test", transport=sync_client.transport) as client:
+                res = await client.get("/api/v1/redis/ping")
+                assert res.status_code == 200
+                data = res.json()
+                assert data["status"] == "OK"
+                assert data["ping"] == "pong"
