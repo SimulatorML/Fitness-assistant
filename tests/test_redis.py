565447b49771a3d77a95fb0f9d7e480e87729449
senkovskiy <senkovskiy@gmail.com>
2025-03-30 22:43:46 +0200

    change redis test

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -1,26 +1,18 @@
 import pytest
 from httpx import AsyncClient
-from fastapi import FastAPI
 from api.app import get_application
-from asgi_lifespan import LifespanManager
-
-
-import pytest
-from httpx import AsyncClient
 from fastapi.testclient import TestClient
-from asgi_lifespan import LifespanManager
-from api.app import get_application
 
 
 @pytest.mark.asyncio
 async def test_redis_ping():
     app = get_application()
 
-    async with LifespanManager(app):
-        with TestClient(app) as sync_client:
-            async with AsyncClient(base_url="http://test", transport=sync_client._transport) as client:
-                res = await client.get("/api/v1/redis/ping")
-                assert res.status_code == 200
-                data = res.json()
-                assert data["status"] == "OK"
-                assert data["ping"] == "pong"
+    # Create a FastAPI test client just to run startup/shutdown events
+    with TestClient(app):
+        async with AsyncClient(base_url="http://test", app=app) as client:
+            res = await client.get("/api/v1/redis/ping")
+            assert res.status_code == 200
+            data = res.json()
+            assert data["status"] == "OK"
+            assert data["ping"] == "pong"
