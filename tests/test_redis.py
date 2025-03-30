20ff627909dc3c294c8104a61292e172bdf4b96b
senkovskiy <senkovskiy@gmail.com>
2025-03-30 22:58:24 +0200

    change redis test, downgrade httpx

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -1,18 +1,18 @@
 import pytest
-from httpx import AsyncClient
+from httpx import AsyncClient, ASGITransport
 from api.app import get_application
-from fastapi.testclient import TestClient
 
 
 @pytest.mark.asyncio
 async def test_redis_ping():
+    
     app = get_application()
 
-    # Create a FastAPI test client just to run startup/shutdown events
-    with TestClient(app):
-        async with AsyncClient(base_url="http://test", app=app) as client:
-            res = await client.get("/api/v1/redis/ping")
-            assert res.status_code == 200
-            data = res.json()
-            assert data["status"] == "OK"
-            assert data["ping"] == "pong"
+    transport = ASGITransport(app=app, lifespan="on")
+    
+    async with AsyncClient(transport=transport, base_url="http://test") as client:
+        res = await client.get("/api/v1/redis/ping")
+        assert res.status_code == 200
+        data = res.json()
+        assert data["status"] == "OK"
+        assert data["ping"] == "pong"
