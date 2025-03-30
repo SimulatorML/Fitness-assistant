4c1621bf6155d9a2253ba63279ba4b9900dfab8a
senkovskiy <senkovskiy@gmail.com>
2025-03-30 19:47:38 +0200

    add saving long history of summaries to posgress

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -1,14 +1,17 @@
 import pytest
 from httpx import AsyncClient
+from asgi_lifespan import LifespanManager
 from api.app import get_application
 
 
 @pytest.mark.asyncio
 async def test_redis_ping():
     app = get_application()
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        res = await client.get("/redis/ping")
-        assert res.status_code == 200
-        data = res.json()
-        assert data["status"] == "OK"
-        assert data["ping"] == "pong"
+
+    async with LifespanManager(app):
+        async with AsyncClient(app=app, base_url="http://test") as client:
+            res = await client.get("/api/v1/redis/ping")
+            assert res.status_code == 200
+            data = res.json()
+            assert data["status"] == "OK"
+            assert data["ping"] == "pong"
